d3a00341cec162516c497d9bc2a312ba3d5eaca9
Remos96 <80366356+Remos96@users.noreply.github.com>
2021-10-29 13:19:31 -0400

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,12 +1,11 @@
 import random
 
-
 print("This password generator contains all uppercase and lower case letters, numbers 0-9, and '!@#$%^&*()_+[]|/;<>[]' symbols.")
 possible_characters = "adcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]|/;<>[]"
 password_size = int(input("Enter the length of characters desired for the password: "))
 password_count = int(input("How many passwords should be created: "))
 
-for i in range(1, password_count + 1):
+for i in range(password_count):
     password = ""
     for j in range(password_size):
         password_character = random.choice(possible_characters)     # Choose from possible_characters random assortment of letters, allows repeats.
